211f08c914e22ff428168d43ba1adcddc1c2ac68
Joseph O'Keefe <josephokeefe@Josephs-Air.home>
2015-08-15 23:13:13 -0400

    Basic start on stat selection page

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 $(function(){
 
-	console.log("test");
+	$('#index-create-button').click(function(){
+		window.location = "stat_selection.html";
+	});
 
-	var x = $(window).scrollTop();
-		console.log(x);
-
-	console.log("test");
 	$(window).scroll(function(){
 		var scroll = $(window).scrollTop();
 		if (scroll > 20){
 			$('#splash-title-bar').addClass('title-bar-scroll');
 			$('#splash-title-bar').removeClass('title-bar-noScroll');
+			$('#index-create-button').addClass('create-button-scroll');
+			$('#index-create-button').removeClass('create-button');
 		}else {
 			$('#splash-title-bar').removeClass('title-bar-scroll');
 			$('#splash-title-bar').addClass('title-bar-noScroll');
+			$('#index-create-button').removeClass('create-button-scroll');
+			$('#index-create-button').addClass('create-button');
 		}		
 	});
+
+
+
 });
